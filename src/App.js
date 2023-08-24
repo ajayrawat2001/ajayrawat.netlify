e751efe2042e1e1d0c654ee8e19dd53c14b8d4c5
Ajay Rawat <129302351+ajayrawat9@users.noreply.github.com>
2023-08-24 16:02:39 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "./component/Pre";
-import Navbar from "./component/Navbar";
+// import React, { useState, useEffect } from "react";
+// import Preloader from "./component/Pre";
+// import Navbar from "./component/Navbar";
 import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Footer from "./component/Footer";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
-import ScrollToTop from "./component/ScrollToTop";
+// import About from "./pages/about/About";
+// import Footer from "./component/Footer";
+// import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+// import ScrollToTop from "./component/ScrollToTop";
 import "./Style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  // const [load, upadateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     upadateLoad(false);
+  //   }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
-    <HashRouter basename="/">
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <Footer />
-      </div>
-    </HashRouter>
+    // <HashRouter basename="/">
+    //   <Preloader load={load} />
+    //   <div className="App" id={load ? "no-scroll" : "scroll"}>
+    //     <Navbar />
+    //     <ScrollToTop />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="*" element={<Navigate to="/" />} />
+    //     </Routes>
+    //     <Footer />
+    //   </div>
+    // </HashRouter>
+    <div className="App">
+      <Home/>
+    </div>
   );
 }
 
