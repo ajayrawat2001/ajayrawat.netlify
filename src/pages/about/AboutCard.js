f159aa25de8f3d8035179777481cc3f467207240
Ajay Rawat <129302351+ajayrawat9@users.noreply.github.com>
2023-09-26 12:27:10 +0530

    changed in about sestion

--- src/pages/about/AboutCard.js
+++ src/pages/about/AboutCard.js
@@ -10,25 +10,27 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Ajay Rawat </span>
             from <span className="purple"> Uttrakhand, India.</span>
-            <br /> I have done B.Sc Computer Science from Himalayan University.
+            <br /> I have done Bachelor of Computer Application from Maharaja
+            Agrasen Himalayan Garhwal University (MAHGU).
             <br />
-            Additionally,  I am Self-taught Frontend
-            engineer with a passion for creating user-friendly and visually
-            appealing websites.
+            Additionally, I am Self-taught Frontend engineer with a passion for
+            creating user-friendly and visually appealing websites.
             <br />
             <br />
             Some of my hobbies are
           </p>
           <ul>
             <li className="about-activity ">
-              <ImPointRight /> Playing online Games
+              <ImPointRight />
+              &nbsp Playing online Games
             </li>
             <li className="about-activity">
               <ImPointRight />
-              Playing Cricket
+              &nbsp Playing Cricket
             </li>
             <li className="about-activity">
-              <ImPointRight /> Traveling
+              <ImPointRight />
+              &nbspTraveling
             </li>
           </ul>
         </blockquote>
